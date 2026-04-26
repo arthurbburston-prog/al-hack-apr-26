44582aab5efb46f5b4dad28f8a2de6fab29bc487
Arthur Burston <arthurbburston@gmail.com>
2026-04-26 14:41:29 +0100

    Remove example pass functionality from

--- pages/example-pass.py
+++ pages/example-pass.py
@@ -1,9 +1,1 @@
-import streamlit as st
 
-st.title("Example Pass")
-st.subheader("This is an example check which always passes")
-
-full_name = st.text_input("Full name")
-
-if full_name != "":
-    st.success("Verified!")
